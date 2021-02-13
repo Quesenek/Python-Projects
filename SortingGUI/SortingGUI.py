78666214875e49e575af162efc72e3f57169a090
Marcus <Quesenekcam@gmail.com>
2021-02-13 06:54:48 -0500

    Fixed bar size issues, and scaling among different bar amounts

--- SortingGUI/SortingGUI.py
+++ SortingGUI/SortingGUI.py
@@ -6,10 +6,10 @@ pygame.init()
 
 windowHeight = 512
 windowWidth = 800
-numAmount = 100
+numAmount = 8
 barSize = int(math.floor(windowWidth / numAmount))
 
-background = (255, 255, 255)
+background = (125, 180, 200)
 
 frameLimit = 0.1
 
@@ -24,8 +24,11 @@ def generateRectangles():
     barArray2 = {}
 
     for i in range(0, (numAmount*barSize), barSize):
-        heightCalc = windowHeight - (windowHeight*((i/barSize)/100))
-        barArray1[(i/barSize)] = (math.floor(i), math.floor((windowHeight*((i/barSize)/100))), barSize, math.floor(heightCalc))
+        if(i == 0):
+            i2=0.5
+            barArray1[(i/barSize)] = (math.floor(i), (windowHeight-((i2)/numAmount)*windowHeight), barSize, (windowHeight-((((i2)/numAmount)*windowHeight)-windowHeight)*-1))
+        else:
+            barArray1[(i/barSize)] = (math.floor(i), (windowHeight-((i/barSize)/numAmount)*windowHeight), barSize, (windowHeight-((((i/barSize)/numAmount)*windowHeight)-windowHeight)*-1))
 
     for i in range(0, numAmount, 1):
         tempArray[i] = barArray1[math.floor((numAmount-1)-i)][0]
@@ -37,16 +40,17 @@ def generateRectangles():
     
     return barArray2
 
+# bar = {}
+# bar[0] = (int(windowHeight-(10/100)*windowHeight), 8, int(windowHeight-(((10/100)*windowHeight)-windowHeight)*-1)) #number working
 
 barArray = generateRectangles()
-print(barArray)
 
 def display():
     global barArray
         
     windowSurface.fill(background)
     for i in range(0, numAmount, 1):
-        pygame.draw.rect(windowSurface, (random.randint(0,254), 0, 0), (i*barSize, barArray[i][1], barArray[i][2], barArray[i][3]))
+        pygame.draw.rect(windowSurface, (100, 0, random.randint(0,254)), (i*barSize, barArray[i][1], barArray[i][2], barArray[i][3]))
     
     #Draw the window onto the screen
     pygame.display.update()
