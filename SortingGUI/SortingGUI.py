8c9301b523f6322a5972d6afd46c6c90b45abbb0
Marcus <Quesenekcam@gmail.com>
2021-02-14 20:24:55 -0500

    Update SortingGUI.py

--- SortingGUI/SortingGUI.py
+++ SortingGUI/SortingGUI.py
@@ -3,27 +3,32 @@ from pygame.locals import *
 import pygame.gfxdraw
 
 
+barArray = []
 
 def pyMain():
     windowHeight = 512
     windowWidth = 800
-    numAmount = 10
+    numAmount = 100
     barSize = int(math.floor(windowWidth / numAmount))
     
-    background = (125, 180, 200) # (125, 180, 200) (0, 0, 0)
-    barColor = [100, 0, None] #[100, 0, None] [69, 69, 69]
+    background = (0, 0, 0) # (125, 180, 200) (0, 0, 0)
+    barColor = [69, None, 69] #[100, 0, None] [69, 69, 69]
     borderColor = [80, 80, 80] #[100, 0, None] [255, 255, 255]
-    frameLimit = 0.1
+    frameLimit = 0.00
 
-    shuffleArray = False
-    border = False
+    shuffleArray = True
+    border = True
     looping = True
+
+    direction = "up"
 
     windowSurface = pyInit(windowWidth, windowHeight)
 
+    global barArray
     barArray = generateRectangles(numAmount, barSize, windowHeight, shuffleArray)
-    display(windowSurface, background, numAmount, barSize, barArray, frameLimit, barColor, borderColor, border)
-    displayParams = [windowSurface, background, numAmount, barSize, barArray, frameLimit , barColor, borderColor, border]
+    # display(windowSurface, background, numAmount, barSize, barArray, frameLimit, barColor, borderColor, border)
+    displayParams = [windowSurface, background, numAmount, barSize, barArray, frameLimit , barColor, borderColor, border, direction]
+    changePos(barArray, displayParams[9], displayParams)
     startLoop(looping, displayParams)
 
 def pyInit(windowWidth, windowHeight):
@@ -41,13 +46,13 @@ def pyInit(windowWidth, windowHeight):
 def generateRectangles(numAmount, barSize, windowHeight, shuffleArray):
 
     barArray = []
-
+    # barArray [index or location, top location, width, height of bar, bar value]
     for i in range(0, (numAmount*barSize), barSize):
         if(i == 0):
             i2=0.5
-            barArray.append([0, (windowHeight-((i2)/numAmount)*windowHeight), barSize, (windowHeight-((((i2)/numAmount)*windowHeight)-windowHeight)*-1)])
+            barArray.append([0, (windowHeight-((i2)/numAmount)*windowHeight), barSize, (windowHeight-((((i2)/numAmount)*windowHeight)-windowHeight)*-1), 0])
         else:
-            barArray.append([0, (windowHeight-((i/barSize)/numAmount)*windowHeight), barSize, (windowHeight-((((i/barSize)/numAmount)*windowHeight)-windowHeight)*-1)])
+            barArray.append([0, (windowHeight-((i/barSize)/numAmount)*windowHeight), barSize, (windowHeight-((((i/barSize)/numAmount)*windowHeight)-windowHeight)*-1), int(i/barSize)])
 
     if(shuffleArray):
         random.shuffle(barArray)
@@ -55,16 +60,26 @@ def generateRectangles(numAmount, barSize, windowHeight, shuffleArray):
     for i in range(0, numAmount, 1):
         barArray[i][0] = i*barSize
 
+    
+
     return barArray
 
 # bar = {}
 # bar[0] = (int(windowHeight-(10/100)*windowHeight), 8, int(windowHeight-(((10/100)*windowHeight)-windowHeight)*-1)) #number working
 
-def display(windowSurface, background, numAmount, barSize, barArray, frameLimit, barColor, borderColor, border):
-            
+def display(windowSurface, background, numAmount, barSize, barArray, frameLimit, barColor, borderColor, border, value):
+    
+    
     windowSurface.fill(background)
     for i in range(0, numAmount, 1):
-        pygame.draw.rect(windowSurface, 
+        if(i == value):
+            pygame.draw.rect(windowSurface, 
+                                        (255, 
+                                         0, 
+                                         0), 
+                                                                                                           (barArray[i][0], barArray[i][1], barArray[i][2], barArray[i][3]))
+        else:        
+            pygame.draw.rect(windowSurface, 
                                         ((barColor[0] if barColor[0] != None else random.randint(0,254)), 
                                          (barColor[1] if barColor[1] != None else random.randint(0,254)), 
                                          (barColor[2] if barColor[2] != None else random.randint(0,254))), 
@@ -80,15 +95,54 @@ def display(windowSurface, background, numAmount, barSize, barArray, frameLimit,
     pygame.display.update()
     time.sleep(frameLimit)
 
+def changePos(array, direction, displayParams):
+    value = 0
+    print(value)
+
+    barsArray = displayParams[4]
+    display(displayParams[0], displayParams[1], displayParams[2], displayParams[3], barsArray, displayParams[5], displayParams[6], displayParams[7], displayParams[8], None)
+
+    time.sleep(2)
+    
+    
+
+    while(True):
+        pygame.event.get()
+        stillSorting = True
+        for i in range (0, displayParams[2]-1, 1):
+            print(barsArray[i][4])
+            
+            if(barsArray[i][4] > barsArray[i+1][4]):
+                stillSorting = False
+                barsArray[i], barsArray[i+1] = barsArray[i+1], barsArray[i]
+
+                for j in range(0, displayParams[2], 1):
+                    barsArray[j][0] = j*displayParams[3]
+
+                displayParams[4] = barsArray
+                display(displayParams[0], displayParams[1], displayParams[2], displayParams[3], displayParams[4], displayParams[5], displayParams[6], displayParams[7], displayParams[8], i)
+        if stillSorting == True:
+            display(displayParams[0], displayParams[1], displayParams[2], displayParams[3], displayParams[4], displayParams[5], displayParams[6], displayParams[7], displayParams[8], None)
+            break
+            
+
+
+    
+
+
 def endGameLoop():
     pygame.quit()
     sys.exit()
 
 def startLoop(looped, displayParams):
+
+    global barArray
+
     #Run the game loop
     while True:
         if (looped == True):
-            display(displayParams[0], displayParams[1], displayParams[2], displayParams[3], displayParams[4], displayParams[5], displayParams[6], displayParams[7], displayParams[8])
+            pass
+            
         for event in pygame.event.get():
             if event.type == QUIT:
                 endGameLoop()
